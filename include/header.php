c05d9dd0181319275d7bd881296780ecf68d9b3b
Clemzer <clement.dumser@orange.fr>
2018-11-11 16:54:16 +0100

    index MAJ view/action bug
    pas optimal, soucis lorsqu'on clique sur le bouton menu depuis la page principale pour faire ses achats. Série d'erreurs liées à un mauvais include. J'ai checké mais je ne trouve pas où est le blem

--- include/header.php
+++ include/header.php
@@ -3,19 +3,19 @@
 	   <a href="main.php"><img id="headerphoto" src="image/logorituelpetit.png" ></a>
     </div>
     <div class="menuCategory">
-		<a id="headertitle" href="main.php">RITUEL</a>
+		<a id="headertitle" href="view/view_main.php">RITUEL</a>
     </div>
 	<div class="menuCategory">
     	<div class="menu"> <input type="text" name="recherche" id="recherche" placeholder="recherche">
     	<input class="menu" type="submit" value="Accepter"></div>
     </div>       
 	<div class="menuCategory">
-		<a class="menu" href="main.php">Menu</a>
+		<a class="menu" href="view/view_main.php">Menu</a>
     </div>
  	<div class="menuCategory">
-    	<a class="menu" href="panier">Panier</a>
+    	<a class="menu" href="view/view_pagepanier">Panier</a>
     </div>
     <div class="menuCategory">
-    	<a class="menu" href="pageconnexion.php">Connexion</a>
+    	<a class="menu" href="view/view_pageconnexion.php">Connexion</a>
     </div>
 </header>
--- index.php
+++ index.php
@@ -27,9 +27,35 @@
 //if 'action/'.$page'.php' exists then include it 
 //(use file_exists($filename) function)
 
-
-
-
+if (isset($_GET["page"])){
+	
+	$page = $_GET["page"];
+	
+	switch($page){
+		case"view/view_pageachat":
+		include'action/action_pageachat.php';
+		break;
+		
+		case"view/view_pageconnexion":
+		include'action/action_pageconnxion.php';
+		break;
+		
+		case"view/view_pageinscription":
+		include'action/action_pageinscription.php';
+		break;
+		
+		case"view/view_inscriptionreussi":
+		include'action/action_inscriptionreussi.php';
+		break;
+		
+		case"view/view_pageachat":
+		include'action/action_pageachat.php';
+		break;
+		
+		default:
+		include('action/action_main.php');
+	}
+}
 
 // TODO insert the start html envelope (<html><head>....</head><body>
 
@@ -51,15 +77,15 @@
     	</div>
             
         <div class="menuCategory">
-			<a href="index.php?page=main">Menu</a>
+			<a href="index.php?page=view/view_main">Menu</a>
     	</div>
 
     	<div class="menuCategory">
-    		<a href="index.php?page=pagepanier">Panier</a>
+    		<a href="index.php?page=view/view_pagepanier">Panier</a>
 		</div>
 
     	<div class="menuCategory">
-    		<a href="index.php?page=pageconnexion">Connexion</a>
+    		<a href="index.php?page=view/view_pageconnexion">Connexion</a>
 		</div>
 
     </nav>
@@ -71,30 +97,64 @@
 //(use file_exists($filename) function)
 //else include 'view/main.php' (it has to exist)
 
+if(isset($_GET["page"])){
+    
+    $page = $_GET["page"];
+	
+	switch($page){
+		case"pagepres":
+		include'pagepres.php';
+		break;
+		
+		case"view/view_pageachat":
+		include'view/view_pageachat.php';
+		break;
+		
+		case"view/view_pageconnexion":
+		include'view/view_pageconnexion.php';
+		break;
+		
+		case"view/view_pageinscription":
+		include'view/view_pageinscription.php';
+		break;
+		
+		case"view/view_inscriptionreussi":
+		include'view/view_inscriptionreussi.php';
+		break;
+		
+		case"view/view_pagepanier":
+		include'view/view_pagepanier.php';
+		break;
+		
+		default:
+		include('view/view_main.php');
+		
+	}	
+}
 
-if (isset($_GET['page']) && $_GET['page'] ='pageachat'){
-	include('pageachat.php');
-	}
-//TODO mettre des else if pour les pages et un seul else pour revenir sur le main
+// if (isset($_GET['page']) && $_GET['page'] ='pageachat'){
+	// include('pageachat.php');
+	// }
+// //TODO mettre des else if pour les pages et un seul else pour revenir sur le main
 
-elseif (isset($_GET['page']) && $_GET['page'] ='pagepres'){
-	$_SESSION['index.php?page=pagepres'] = htmlentities($_GET['index.php?page=pagepres']);
-}
-elseif (isset($_GET['page']) && $_GET['page'] ='pageinscription'){
-	$_SESSION['index.php?page=pageinscription'] = htmlentities($_GET['index.php?page=pageinscription']);
-}
-elseif (isset($_GET['page']) && $_GET['page'] ='pageconnexion'){
-	$_SESSION['index.php?page=pageconnexion'] = htmlentities($_GET['index.php?page=pageconnexion']);
-}
-elseif (isset($_GET['page']) && $_GET['page'] = 'inscriptionreussi'){
-	$_SESSION['index.php?page=inscriptionreussi'] = htmlentities($_GET['index.php?page=inscriptionreussi']);
-}
-elseif (isset($_GET['page']) && $_GET['page'] = 'pagepanier'){
-	$_SESSION['index.php?page=pagepanier'] = htmlentities($_GET['index.php?page=pagepanier']);
-}
-else{
-	include('main.php');
-}
+// elseif (isset($_GET['page']) && $_GET['page'] ='pagepres'){
+	// $_SESSION['index.php?page=pagepres'] = htmlentities($_GET['index.php?page=pagepres']);
+// }
+// elseif (isset($_GET['page']) && $_GET['page'] ='pageinscription'){
+	// $_SESSION['index.php?page=pageinscription'] = htmlentities($_GET['index.php?page=pageinscription']);
+// }
+// elseif (isset($_GET['page']) && $_GET['page'] ='pageconnexion'){
+	// $_SESSION['index.php?page=pageconnexion'] = htmlentities($_GET['index.php?page=pageconnexion']);
+// }
+// elseif (isset($_GET['page']) && $_GET['page'] = 'inscriptionreussi'){
+	// $_SESSION['index.php?page=inscriptionreussi'] = htmlentities($_GET['index.php?page=inscriptionreussi']);
+// }
+// elseif (isset($_GET['page']) && $_GET['page'] = 'pagepanier'){
+	// $_SESSION['index.php?page=pagepanier'] = htmlentities($_GET['index.php?page=pagepanier']);
+// }
+// else{
+	// include('main.php');
+// }
 
 // pour les includes de pages, les écrire sous la forme index.php?page = "nom page"
 // TODO insert the end html envelope (</body></html>)
